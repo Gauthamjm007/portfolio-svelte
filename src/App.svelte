8bcb3043d6ebd54e79f96e0089be455ae4c1fe5c
Gauthamjm007 <j.mgautham15@gmail.com>
2022-07-02 00:46:28 +0530

    updating repo

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import logo from './assets/svelte.png'
+  import TailwindCss from './TailwindCSS.svelte';
+  import Main from './lib/Main.svelte'
+</script>
+
+<TailwindCss />
+<Main/>
+
+
+
