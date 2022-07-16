789ac935993ffbe72a0c065276bfd6bc55f0d2f1
Gauthamjm007 <j.mgautham15@gmail.com>
2022-07-16 14:27:56 +0530

    updating company details

--- src/lib/Main.svelte
+++ src/lib/Main.svelte
@@ -1,9 +1,11 @@
 <script>
   import Intro from "./Intro.svelte";
+  import Work from "./Work.svelte";
 </script>
 
 <main class="w-full min-h-screen font-sans bg-black">
     <div class="container px-6 py-4 mx-auto text-white md:py-8 md:px-14 lg:py-0 lg:flex">
         <Intro/>
+        <Work/>
     </div>
 </main>
---
+++ src/lib/Work.svelte
@@ -0,0 +1,94 @@
+<div class="flex-1 lg:py-20 lg:h-screen lg:overflow-y-scroll">
+    <div id="experience" class="pt-14"></div>
+    <h1 class="lg:hidden">
+        Experience{" "}
+    <div class="h-0.5 w-1/2 bg-dawn rounded-full"></div>
+    </h1>
+
+    <div class="flex-col items-center mb-20 space-y-14 lg:flex lg:mb-24">
+        <div class="relative p-6 my-3 text-gray-200 transition duration-300 transform rounded-lg bg-dusk lg:w-2/3 lg:hover:scale-110">
+        <!-- svelte-ignore invalid-html-attribute -->
+        <img
+            src="/tcs.png"
+            alt="tcs"
+            class="absolute scale-50 -top-24 left-0 transform -translate-x-32 -translate-y-8 shadow"
+            />
+        <p class="text-xl font-medium">
+            <span>Software Engineer</span> @{" "}
+            <a
+                href="https://www.tcs.com/"
+                target="_blank"
+                class="text-blue-400 underline"
+            >
+                TCS
+            </a>
+        </p>
+        <p class="text-sm text-gray-400">2018-2019</p>
+        <div class="mt-4 space-y-2 tracking-wide text-justify lg">
+            <p>
+            TCS{" "}
+            <span role="img" alt="cloud emoji">
+                ☁️
+            </span>{" "}
+            Tata Consultancy Services is an Indian multinational information technology services and consulting company with its headquarters in Mumbai. It is a part of the Tata Group and operates in 149 locations across 46 countries.</p>
+        </div>
+        </div>
+    </div>
+    <div class="flex-col items-center mb-20 space-y-14 lg:flex lg:mb-24">
+        <div class="relative p-6 my-3 text-gray-200 transition duration-300 transform rounded-lg bg-dusk lg:w-2/3 lg:hover:scale-110">
+        <!-- svelte-ignore invalid-html-attribute -->
+        <img
+            src="/ds.jpeg"
+            alt="tcs"
+            class="absolute scale-50 -top-24 left-0 transform -translate-x-6 -translate-y-12 shadow"
+            />
+        <p class="text-xl font-medium">
+            <span>Assosiate Software Engineer</span> @{" "}
+            <a
+                href="https://www.tcs.com/"
+                target="_blank"
+                class="text-blue-400 underline"
+            >
+                TCS
+            </a>
+        </p>
+        <p class="text-sm text-gray-400">2018-2019</p>
+        <div class="mt-4 space-y-2 tracking-wide text-justify lg">
+            <p>
+            TCS{" "}
+            <span role="img" alt="cloud emoji">
+                ☁️
+            </span>{" "}
+            Tata Consultancy Services is an Indian multinational information technology services and consulting company with its headquarters in Mumbai. It is a part of the Tata Group and operates in 149 locations across 46 countries.
+        </div>
+        </div>
+    </div>
+    <h1 class="lg:hidden">
+        Skills <div class="h-0.5 w-1/2 bg-dawn rounded-full"></div>
+    </h1>
+    <h1 class="lg:hidden">
+        Contact me{" "}
+        <div class="h-0.5 w-1/2 bg-dawn rounded-full"></div>
+    </h1>
+    <div class="pb-14">
+        <p class="text-sm text-center mt-9">
+            Build using{" "}
+            <a
+            href="https://svelte.dev/"
+            target="_blank"
+            class="text-gray-400 underline"
+            >
+            Svelte
+            </a>
+            and{" "}
+            <a
+            href="https://tailwindcss.com/"
+            target="_blank"
+            class="text-gray-400 underline"
+            >
+            Tailwind css
+            </a>{" "}
+            .
+        </p>
+    </div>
+    </div>
